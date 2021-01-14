8ed3fb5a1cc32355d4eabc4ea654048eda19a3ce
bobomurod <bobomurod.boboev@yandex.ru>
2021-01-14 23:57:36 +0500

    small refactorontr

--- libs/backend/users/services/src/lib/user-activation-service/user-activation.service.ts
+++ libs/backend/users/services/src/lib/user-activation-service/user-activation.service.ts
@@ -2,10 +2,12 @@ import {UserService} from "../../../../../../../apps/backend/src/app/modules/use
 import {UserDto, UserWhereDto} from "@mobile-control-gateway/backend/users/backend/class-transfer-objects";
 import {UserCodeDto} from "@mobile-control-gateway/backend/users/backend/class-transfer-objects";
 import {SendSmsService} from "../send-sms-service/send-sms.service";
-import {Injectable} from "@nestjs/common";
+import {Injectable, Logger} from "@nestjs/common";
 
 @Injectable()
 export class UserActivationService {
+  private readonly _logger: Logger = new Logger('userActivationService')
+
   constructor(
     private readonly _userService: UserService,
     private readonly _sendSmsService: SendSmsService
@@ -29,6 +31,7 @@ export class UserActivationService {
 
   async checkActivationCode(activationData: UserCodeDto): Promise<boolean> {
     const userFromDb = await this._userService.getSingle({_id: activationData._id})
+    this._logger.log(userFromDb)
     if (userFromDb.approveCode === activationData.activationCode)
     {
       await this._activate(userFromDb._id)
